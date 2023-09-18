b32baa8fa83af093c329915164bd73d6c0a1e604
Jack DeMeyers <319723@wegmans.com>
2023-09-17 21:03:23 -0400

    fix local config

--- src/tokenReplace.js
+++ src/tokenReplace.js
@@ -2,7 +2,5 @@ const fs = require('fs')
 
 const configFile = 'env.tokenized.js'
 let configContent = fs.readFileSync(configFile, 'utf8')
-
 configContent = configContent.replace('__STORAGECONN__', process.env.STORAGECONN)
-
 fs.writeFileSync(configFile, configContent, 'utf8')
