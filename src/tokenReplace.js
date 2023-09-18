062af57ddb442150805175273c3dac4f2f3406c4
Jack DeMeyers <319723@wegmans.com>
2023-09-17 21:40:21 -0400

    fix token replace

--- src/tokenReplace.js
+++ src/tokenReplace.js
@@ -4,9 +4,9 @@ const serverConfig = 'env_server.tokenized.js'
 let serverConfigContent = fs.readFileSync(serverConfig, 'utf8')
 // replacements
 serverConfigContent = serverConfigContent.replace('__STORAGECONN__', process.env.STORAGECONN)
-fs.writeFileSync(serverConfig, configContent, 'utf8')
+fs.writeFileSync(serverConfig, serverConfigContent, 'utf8')
 
 const webConfig = 'env_web.tokenized.js'
 let webConfigContent = fs.readFileSync(webConfig, 'utf8')
 // replacements
-fs.writeFileSync(webConfig, configContent, 'utf8')
+fs.writeFileSync(webConfig, webConfigContent, 'utf8')
